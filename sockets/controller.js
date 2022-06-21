452580d4af5868696549136e7f1f8d7f87fef9fe
juan daniel suarez amado <62775732+jdvpl@users.noreply.github.com>
2022-06-20 21:33:25 -0500

    socket to emit if the user updated the task

--- sockets/controller.js
+++ sockets/controller.js
@@ -6,11 +6,17 @@ const socketController=(socket) =>{
   socket.on('open project',(project) =>{
     socket.join(project)
   })
-
   socket.on('new task',(task) =>{
     const project = task.project;
-    console.log(project)
     socket.to(project).emit('task added',task)
+  })
+  socket.on('delete task',task =>{
+    const project = task.project;
+    socket.to(project).emit('task-deleted',task)
+  })
+  socket.on('update-task',task =>{
+    const project = task.project._id;
+    socket.to(project).emit('task-updated',task)
   })
 
 
