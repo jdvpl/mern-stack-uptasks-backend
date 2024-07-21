fd9e7341a09b0527af50e675b1417cf1849ef923
jdsuarez <jdsuarez@fgs.co>
2024-07-21 18:08:01 -0500

    adding the correct urls

--- models/server.js
+++ models/server.js
@@ -3,81 +3,82 @@ const cors = require('cors');
 const { dbConection } = require('../database/database');
 const { socketController } = require('../sockets/controller');
 
-class Server{
-  constructor(){
-    this.app = express();
-    this.port = process.env.PORT || 3000;
-    this.server=require('http').createServer(this.app);
-    this.io=require('socket.io')(this.server,{
-      pingTimeout:60000,
-      cors:{
-        origin: process.env.F1_URL
-      }
-    })
-    this.paths={
-      users:'/api/users',
-      auth:'/api/auth',
-      projects:'/api/projects',
-      tasks:'/api/tasks',
-    }
-
-    // conectar a la base de datos
-    this.conectarDb()
-
-    // middleware: son funcionalidads para el webserver
-    this.middlewares();
-    // rutas de mi app
-    this.routes();
-
-    // sockets
-    this.sockets();
-  }
-
-
-  routes() {
-    this.app.use(this.paths.users, require('../routes/users.routes'))
-    this.app.use(this.paths.auth, require('../routes/auth.routes'))
-    this.app.use(this.paths.projects, require('../routes/project.routes'))
-    this.app.use(this.paths.tasks, require('../routes/tasks.routes'))
-
-  }
-  sockets(){
-    this.io.on('connection',socketController)
-  }
-  listen() {
-    this.server.listen(this.port, () => {
-      console.log(`Corriendo http://localhost:${this.port}`)
-    });
-  }
-
-  async conectarDb(){
-    await dbConection()
-  }
-
-  middlewares() {
-    const urls=process.env.URLS_WHITE_LIST.split(',')
-    const whiteList=urls;
-    const corsOptions = {
-      origin:function(origin,callback){
-        if(whiteList.includes(origin)){
-          callback(null,true);
-        }else{
-          callback(new Error(`No esta permitido para esta url. ${origin}`));
+class Server {
+    constructor() {
+        this.app = express();
+        this.port = process.env.PORT || 3000;
+        this.server = require('http').createServer(this.app);
+        this.io = require('socket.io')(this.server, {
+            pingTimeout: 60000,
+            cors: {
+                origin: process.env.F1_URL
+            }
+        })
+        this.paths = {
+            users: '/api/users',
+            auth: '/api/auth',
+            projects: '/api/projects',
+            tasks: '/api/tasks',
         }
-      }
+
+        // conectar a la base de datos
+        this.conectarDb()
+
+        // middleware: son funcionalidads para el webserver
+        this.middlewares();
+        // rutas de mi app
+        this.routes();
+
+        // sockets
+        this.sockets();
     }
-    // usar cors
-    if(process.env.ENV_PRODUCTION=="production"){
-      this.app.use(cors(corsOptions));
-    }else{
-      this.app.use(cors());
+
+
+    routes() {
+        this.app.use(this.paths.users, require('../routes/users.routes'))
+        this.app.use(this.paths.auth, require('../routes/auth.routes'))
+        this.app.use(this.paths.projects, require('../routes/project.routes'))
+        this.app.use(this.paths.tasks, require('../routes/tasks.routes'))
+
+    }
+    sockets() {
+        this.io.on('connection', socketController)
+    }
+    listen() {
+        this.server.listen(this.port, () => {
+            console.log(`Corriendo http://localhost:${this.port}`)
+        });
     }
 
-    // parseo de la info del body
-    this.app.use(express.json())
+    async conectarDb() {
+        await dbConection()
+    }
+
+    middlewares() {
+        const urls = process.env.URLS_WHITE_LIST.split(',')
+        console.log({ urls })
+        const whiteList = urls;
+        const corsOptions = {
+                origin: function(origin, callback) {
+                    if (whiteList.includes(origin)) {
+                        callback(null, true);
+                    } else {
+                        callback(new Error(`No esta permitido para esta url. ${origin}`));
+                    }
+                }
+            }
+            // usar cors
+        if (process.env.ENV_PRODUCTION == "production") {
+            this.app.use(cors(corsOptions));
+        } else {
+            this.app.use(cors());
+        }
+
+        // parseo de la info del body
+        this.app.use(express.json())
 
 
-  }
+    }
 }
 
 module.exports = Server;
